81947202bb051398f757c245f0e21ad05f858926
Shane McKee <shane.mckee@canonical.com>
2024-07-22 18:54:01 +0800

    Initial commit

---
+++ upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/linux-npu-driver/issues
+# Bug-Submit: https://github.com/<user>/linux-npu-driver/issues/new
+# Changelog: https://github.com/<user>/linux-npu-driver/blob/master/CHANGES
+# Documentation: https://github.com/<user>/linux-npu-driver/wiki
+# Repository-Browse: https://github.com/<user>/linux-npu-driver
+# Repository: https://github.com/<user>/linux-npu-driver.git
---
+++ watch.ex
@@ -0,0 +1,2 @@
+version=4
+https://github.com/intel/linux-npu-driver/archive/refs/tags/v1.5.1.tar.gz
